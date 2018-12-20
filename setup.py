d0d6c080baab5e261dbce8587f0abbbf6c888fb2
Oleg Utkin <oleg.utkin@uber.com>
2018-12-19 16:17:18 -0800

    Improve info in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="fsearch",
     version="0.0.1",
+    description="A fuzzy matcher based on fzf.",
+    author="nonlogicaldev",
+    url="https://github.com/NonLogicalDev/nld.cli.fsearch",
 
     packages=find_packages(),
     scripts=[
